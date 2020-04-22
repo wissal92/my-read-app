f6d22edbbda694a54256cecd51799c8dcb8232e4
wissal <wissal.ouarsafi@gmail.com>
2020-04-22 13:57:54 -0700

    wrap the app with the provider, Consume it in the needed component , Spread it so that i can pass it easly in other components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
+import Provider, {BookContext} from './context/BookProvider';
 import Library from './pages/Library';
 import BookSearch from './pages/BookSearch';
 import './App.css';
@@ -8,10 +9,12 @@ class BooksApp extends React.Component {
 
   render() {
     return (
+      <Provider>
         <Switch className="app">
-          <Route exact path='/' component={Library} />
-          <Route exact path='/search' component={BookSearch} />
+          <Route exact path='/'  render={() => <BookContext.Consumer>{context => <Library {...context} />} </BookContext.Consumer>} />
+          <Route exact path='/search'  render={() => <BookContext.Consumer>{context => <BookSearch {...context}/>}  </BookContext.Consumer>} />
         </Switch>
+      </Provider>
     )
   }
 }
