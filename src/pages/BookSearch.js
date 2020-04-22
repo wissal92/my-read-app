8b3ed24e2d9206320e2571f98d7a9937067c3768
wissal <wissal.ouarsafi@gmail.com>
2020-04-22 13:40:08 -0700

    Fix a bug

--- src/pages/BookSearch.js
+++ src/pages/BookSearch.js
@@ -8,7 +8,7 @@ class BookSearch extends Component {
               <div className="search-books-bar">
                 <Link to='/' className="close-search">Close</Link>
                 <div className="search-books-input-wrapper">
-                  <input type="text" placeholder="Search by title or author" onChange={this.handleChange} value={this.state.query}/>
+                  <input type="text" placeholder="Search by title or author"/>
                 </div>
               </div>
             </div>
