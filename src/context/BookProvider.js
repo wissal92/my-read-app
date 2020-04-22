df389bc288bc9a756f4f994f3eb1cc6a85f081b0
wissal <wissal.ouarsafi@gmail.com>
2020-04-22 13:46:48 -0700

    create a context folder that contains a file that is  containing the important state in our app with the needed functions to change state

---
+++ src/context/BookProvider.js
@@ -0,0 +1,29 @@
+import React, { Component, createContext } from 'react';
+
+export const BookContext = createContext();
+
+export default class BookProvider extends Component{
+   constructor(props){
+       super(props);
+       this.state = {
+           books: [],
+           currentlyReading: [],
+           wantToRead: [],
+           read: [],
+           organizeBooks: books => {
+               const currentlyReading = books.filter(book => book.shelf === 'currentlyReading');
+               const wantToRead = books.filter(book => book.shelf === 'wantToRead');
+               const read = books.filter(book => book.shelf === 'read');
+               this.setState({books, currentlyReading, wantToRead, read});
+           }
+       }
+   }
+
+   render(){
+       return(
+           <BookContext.Provider value={{...this.state}}>
+               {this.props.children}
+           </BookContext.Provider>
+       )
+   }
+}
